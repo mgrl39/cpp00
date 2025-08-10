16cdef12da9b36b71ecbcd818758eca8e8f24e3f
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-08-10 16:17:24 +0200

    add megaphone.cpp and Makefile

--- ex00/playing-with-strings.cpp
+++ ex00/playing-with-strings.cpp
@@ -6,7 +6,7 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/10 10:09:24 by meghribe          #+#    #+#             */
-/*   Updated: 2025/08/10 13:31:23 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/08/10 13:30:02 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 # define DEFAULT_MSG "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
 
+/*
 void	convertAndPrint(int argc, char *argv[])
 {
 	int	i;
@@ -27,13 +28,33 @@ void	convertAndPrint(int argc, char *argv[])
 			std::cout << (char)std::toupper(argv[i][j++]);
 		i++;
 	}
+}*/
+
+std::string convert(int argc, char *argv[])
+{
+	std::string args;
+	int		i;
+
+	args = "";
+	i = 1;
+	while (i < argc)
+	{
+		args += argv[i];
+		i++;
+	}
+	return (args);
 }
+
 int	main(int argc, char *argv[])
 {
-
+	//std::cout << convert(argc, argv);
+	std::cout << (argc < 2 ? DEFAULT_MSG : convert(argc, argv)) << std::endl;
+	return (0);
+	/*
 	if (argc < 2)
 		std::cout << DEFAULT_MSG;
 	else
 		convertAndPrint(argc, argv);
+		*/
 	return (std::cout << std::endl, 0);
 }
