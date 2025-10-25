57a7a10e84b91c98dadb43d344ea4eb0e9e8aa39
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-10-25 08:30:51 +0200

    add simple welcome message

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,14 +6,19 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 08:09:08 by meghribe          #+#    #+#             */
-/*   Updated: 2025/10/25 08:20:04 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/10/25 08:30:06 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
 
+void	print_welcome(void)
+{
+	std::cout << "PHONE BOOK" << std::endl;
+}
 int	main(int argc, char *argv[])
 {
+	print_welcome();
 	return (0);
 }
