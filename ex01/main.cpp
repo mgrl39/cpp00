fbcaa189cafb223bbcce87c5116beff23cde7249
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-10-25 08:59:17 +0200

    add available orders in c++

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 08:09:08 by meghribe          #+#    #+#             */
-/*   Updated: 2025/10/25 08:38:46 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/10/25 08:58:54 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,35 @@
 
 void	print_welcome(void)
 {
-	std::string welcome;
+	std::string	welcome;
 
 	welcome = "\e[0;31m░█▀█░█░█░█▀█░█▀█░█▀▀░█▀▄░█▀█░█▀█░█░█\n\
 ░█▀▀░█▀█░█░█░█░█░█▀▀░█▀▄░█░█░█░█░█▀▄\n\
 ░▀░░░▀░▀░▀▀▀░▀░▀░▀▀▀░▀▀░░▀▀▀░▀▀▀░▀░▀\n\e[0m";
 	std::cout << welcome << std::endl;
 }
+
+void	print_available_orders(void)
+{
+	std::string	order_list;
+
+	order_list = "\e[1;33mORDERS \e[0;32m▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰\n";
+	order_list += "\e[1;32mADD\e[0m: save a new contact\n";
+	order_list += "\e[1;32mSEARCH\e[0m: display a specific contact\n";
+	order_list += "\e[1;32mEXIT\e[0m: the program quits\n";
+	order_list += "\e[0;32m▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰\n\e[0m";
+	std::cout << order_list << std::endl;
+}
+
 int	main(int argc, char *argv[])
 {
+	std::string	order;
+
 	print_welcome();
+	while (order != "EXIT")
+	{
+		print_available_orders();
+		order = "EXIT";
+	}
 	return (0);
 }
