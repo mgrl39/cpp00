0d2cd5f3c1214d07612ff71d71c567a7d29ec798
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-10-25 08:39:16 +0200

    add color phonebook

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 08:09:08 by meghribe          #+#    #+#             */
-/*   Updated: 2025/10/25 08:30:06 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/10/25 08:38:46 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,12 @@
 
 void	print_welcome(void)
 {
-	std::cout << "PHONE BOOK" << std::endl;
+	std::string welcome;
+
+	welcome = "\e[0;31m░█▀█░█░█░█▀█░█▀█░█▀▀░█▀▄░█▀█░█▀█░█░█\n\
+░█▀▀░█▀█░█░█░█░█░█▀▀░█▀▄░█░█░█░█░█▀▄\n\
+░▀░░░▀░▀░▀▀▀░▀░▀░▀▀▀░▀▀░░▀▀▀░▀▀▀░▀░▀\n\e[0m";
+	std::cout << welcome << std::endl;
 }
 int	main(int argc, char *argv[])
 {
