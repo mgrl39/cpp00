8e6e0df8523f7f8669d4bb51f55eee5dec839367
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-10-25 11:07:08 +0200

    add more data

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -9,11 +9,16 @@ void	PhoneBook::add_contact()
 	std::string phone_number;
 	std::string darkest_secret;
 
-	std::cout << "\e[1;33mFIRST NAME >\e[0m ";
+	// TODO: A saved contact can’t have empty fields
+	std::cout << "\e[1;33mFIRST NAME >\e[0m "; 
 	std::getline(std::cin, first_name);
+	std::cout << "\e[1;33mLAST NAME >\e[0m "; 
 	std::getline(std::cin, last_name);
+	std::cout << "\e[1;33mNICKNAME >\e[0m "; 
 	std::getline(std::cin, nickname);
+	std::cout << "\e[1;33mPHONE NUMBER >\e[0m "; 
 	std::getline(std::cin, phone_number);
+	std::cout << "\e[1;33mDARKEST SECRET >\e[0m "; 
 	std::getline(std::cin, darkest_secret);
 
 	Contact	new_contact(first_name, last_name, nickname,\
