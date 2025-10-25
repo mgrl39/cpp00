6b397af17e22d348dcf914f31211c57c00db75dd
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-10-25 09:36:55 +0200

    add Contact.hpp

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,11 +6,13 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 08:22:56 by meghribe          #+#    #+#             */
-/*   Updated: 2025/10/25 09:20:12 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/10/25 09:35:34 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#include <string>
 
 class Contact {
 	private:
@@ -19,4 +21,8 @@ class Contact {
 		std::string nickname;
 		std::string phone_number;
 		std::string darkest_secret;
+	public:
+		Contact(std::string first_name, std::string last_name, \
+				std::string nick_name, std::string phone_number\
+				, std::string darkest_secret);
 };
