419c8a895aaf2e7b8f943911f04e99ba98dd0813
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-10-26 09:05:03 +0100

    add getters and setters for contact

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 07:50:42 by meghribe          #+#    #+#             */
-/*   Updated: 2025/10/26 07:50:42 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/10/26 09:03:56 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,33 @@ Contact::Contact(std::string first_name, std::string last_name \
 
 Contact::Contact()
 {
+}
+
+Contact::~Contact()
+{
+}
+
+std::string	Contact::get_first_name(void)
+{
+	return (this->first_name);
+}
+
+std::string Contact::get_last_name(void)
+{
+	return (this->last_name);
+}
+
+std::string	Contact::get_nickname(void)
+{
+	return (this->nickname);
+}
+
+std::string	Contact::get_phone_number(void)
+{
+	return (this->phone_number);
+}
+
+std::string	Contact::get_darkest_secret(void)
+{
+	return (this->darkest_secret);
 }
--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 08:22:56 by meghribe          #+#    #+#             */
-/*   Updated: 2025/10/25 10:57:12 by meghribe         ###   ########.fr       */
+/*   Updated: 2025/10/26 09:03:46 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,21 @@ class Contact {
 		std::string phone_number;
 		std::string darkest_secret;
 	public:
-		Contact();
+		Contact(void);
+		~Contact(void);
 		Contact(std::string first_name, std::string last_name, \
 				std::string nick_name, std::string phone_number\
 				, std::string darkest_secret);
+
+		std::string	get_first_name(void);
+		std::string	get_last_name();
+		std::string	get_nickname();
+		std::string	get_phone_number();
+		std::string	get_darkest_secret();
+
+		void		set_first_name(std::string first_name);
+		void		set_last_name(std::string last_name);
+		void		set_nickname(std::string nickname);
+		void		set_phone_number(std::string phone_number);
+		void		set_darkest_secret(std::string darkest_secret);
 };
