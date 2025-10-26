6df6ae8ccbb396320a9b6cd945f04faa9149ff77
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-10-26 06:20:31 +0100

    comented value_saver

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,12 +6,40 @@ static void	value_saver(std::string msg, std::string *value)
 {
 	std::cout << "\e[1;33m";
 	std::cout << msg << ">\e[0m ";
-	while (std::getline(std::cin, *value))
+	int	i;
+
+	i = 0;
+	//while (!std::getline(std::cin, *value))
+	while (1)
 	{
-		break ;
+		if (!std::getline(std::cin, *value)) 
+		{
+			std::cin.clear();
+			std::cout << "fail" << std::endl;
+		}
+		std::cout << (*value).length() << std::endl;
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore((*value).size());
+			std::cout << "fail" << std::endl;
+		}
+		if (std::cin.eof()) 
+		{
+			std::cout << "EOF" << std::endl;
+			std::cout << i;
+			std::cin.clear();
+			std::cin.ignore((*value).size());
+		}
+		*value = "";
+		//if ((*value).length() > 0)
+			//break ;
 		std::cout << "\e[1;33m";
 		std::cout << msg << ">\e[0m ";
+		i++;
 	}
+	if ((*value).empty())
+		std::cout << "MENSAJEEE" << std::endl;
 	std::cout << std::endl;
 }
 */
@@ -27,7 +55,14 @@ void	PhoneBook::add_contact()
 	// TODO: A saved contact can’t have empty fields
 	std::cout << "\e[1;33mFIRST NAME >\e[0m "; 
 	std::getline(std::cin, first_name);
+	if (std::cin.fail())
+	{
+		std::cin.clear();
+		std::cout << "FAIL" << std::endl;
+	}
+	//value_saver("FIRST NAME", &first_name);
 	//std::getline(std::cin, first_name);
+	/*
 	std::cout << "\e[1;33mLAST NAME >\e[0m "; 
 	std::getline(std::cin, last_name);
 	std::cout << "\e[1;33mNICKNAME >\e[0m "; 
@@ -35,7 +70,7 @@ void	PhoneBook::add_contact()
 	std::cout << "\e[1;33mPHONE NUMBER >\e[0m "; 
 	std::getline(std::cin, phone_number);
 	std::cout << "\e[1;33mDARKEST SECRET >\e[0m "; 
-	std::getline(std::cin, darkest_secret);
+	std::getline(std::cin, darkest_secret);*/
 
 	Contact	new_contact(first_name, last_name, nickname,\
 		phone_number, darkest_secret);
